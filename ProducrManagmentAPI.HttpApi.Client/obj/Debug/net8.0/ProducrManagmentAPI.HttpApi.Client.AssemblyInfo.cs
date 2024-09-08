bd4a1834ad0f661ea89f38509fe8b0f429296514
Ahmed Abdelghafar <aabdelghafar@ejada.com>
2024-09-08 16:11:42 +0300

    Committing all changes

--- ProducrManagmentAPI.HttpApi.Client/obj/Debug/net8.0/ProducrManagmentAPI.HttpApi.Client.AssemblyInfo.cs
+++ ProducrManagmentAPI.HttpApi.Client/obj/Debug/net8.0/ProducrManagmentAPI.HttpApi.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProducrManagmentAPI.HttpApi.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cea4b9212c9732f623bea0e665229514d958174b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6748fcc266b5f8cebb91a9a97e86e45aaa200a27")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProducrManagmentAPI.HttpApi.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProducrManagmentAPI.HttpApi.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
